420eadb1566758695d656de893b41be0b9be82dc
StepanLovyagin <lovyagin13@gmail.com>
2025-12-18 14:14:37 +0300

    обновлён скрипт build

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import subprocess
 import platform
+import sys
 
 DIST = "dist"
 FILES = ["index.html", "assembler.py", "interpreter.py"]
@@ -12,7 +13,9 @@ def main():
     #build web
     web_dir = os.path.join(DIST, "web")
     os.makedirs(web_dir)
-    for f in FILES: shutil.copy(f, os.path.join(web_dir, f))
+    for f in FILES: 
+        if os.path.exists(f):
+            shutil.copy(f, os.path.join(web_dir, f))
     print(f"Web build: {web_dir}")
 
     #build desktop
@@ -23,13 +26,15 @@ def main():
     
     try:
         subprocess.run([
-            "py", "-m", "PyInstaller", "--noconsole", "--onefile", 
+            sys.executable, "-m", "PyInstaller", "--noconsole", "--onefile", 
             "--name", exe_name,
             "--distpath", desktop_dir,
             "uvm_gui.py"
         ], check=True)
+        
         if os.path.exists("vector_mul_5x6.csv"):
             shutil.copy("vector_mul_5x6.csv", os.path.join(desktop_dir, "vector_mul_5x6.csv"))
+            
         print(f"Desktop build успешен!")
     except Exception as e:
         print(f"Desktop build провален (PyInstaller missing?): {e}")
